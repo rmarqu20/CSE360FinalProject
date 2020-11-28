86de58c2a5e7e3b4558c4719a0f68403c9939576
Richard17256 <61338064+Richard17256@users.noreply.github.com>
2020-11-27 23:12:32 -0700

    Implemented prettyString and getPercent functions.

--- MainProject/src/DataPlot.java
+++ MainProject/src/DataPlot.java
@@ -1,4 +1,9 @@
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
@@ -11,8 +16,11 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 
-public class DataPlot extends JFrame
+public abstract class DataPlot extends JFrame implements Observer
 {
+	//private static final long serialVersionUID = 6294689542092367723L;
+	Students stud;
+	
 	public void ScatterPlot(String title)
 	{
 		//super(title);
@@ -32,23 +40,29 @@ public class DataPlot extends JFrame
 	{
 		XYSeriesCollection dataset = new XYSeriesCollection();
 		
-		XYSeries series1 = new XYSeries("Date1");
+		ArrayList<Student> studList = stud.getStudents();
+		ArrayList<Date> dates = stud.getDates();
 		
-		//Students.getStudents(); //??
+		int listSize = studList.size();
+		int datesSize = dates.size();
+		int studCount;
+		int percCount = 0;
+		int temp; 
 		
-		series1.add(1, 72.9);  
-	    series1.add(2, 81.6);  
-	    series1.add(3, 88.9);  
-	    series1.add(4, 96);  
-	    series1.add(5, 102.1);  
-	    series1.add(6, 108.5);  
-	    series1.add(7, 113.9);  
-	    series1.add(8, 119.3);  
-	    series1.add(9, 123.8);  
-	    series1.add(10, 124.4); 
-	    
-	    dataset.addSeries(series1);
-	    
+		
+		ArrayList<XYSeries> arrSeries = new ArrayList<XYSeries>();
+		
+		for(int i = 0; i < datesSize; i++)
+		{
+			arrSeries.add(new XYSeries(prettyString(dates.get(i))));
+			
+			for(int j = 0; j < listSize; j++)
+			{
+				//series1.add(i, j);
+			}
+		}
+		
+	    //example
 	    XYSeries series2 = new XYSeries("Date2");
 	    series2.add(1, 72.5);  
 	    series2.add(2, 80.1);  
@@ -61,8 +75,83 @@ public class DataPlot extends JFrame
 	    series2.add(9, 122.9);  
 	    series2.add(10, 123.4); 
 	    
-	    dataset.addSeries(series2);
+	    dataset.addSeries(series2); //how to add series to collection
 	    
 	    return dataset;
+	}
+	
+	//function that converts dates to correctly formatted strings
+	@SuppressWarnings("deprecation")
+	public String prettyString(Date i)
+	{
+		String day = Integer.toString(i.getDay());
+		
+		if(i.getMonth() == 1)
+		{
+			return "Jan " + day;
+		}
+		if(i.getMonth() == 2)
+		{
+			return "Feb " + day;
+		}
+		if(i.getMonth() == 3)
+		{
+			return "Mar " + day;
+		}
+		if(i.getMonth() == 4)
+		{
+			return "Apr " + day;
+		}
+		if(i.getMonth() == 5)
+		{
+			return "May " + day;
+		}
+		if(i.getMonth() == 6)
+		{
+			return "Jun " + day;
+		}
+		if(i.getMonth() == 7)
+		{
+			return "Jul " + day;
+		}
+		if(i.getMonth() == 8)
+		{
+			return "Aug " + day;
+		}
+		if(i.getMonth() == 9)
+		{
+			return "Sep " + day;
+		}
+		if(i.getMonth() == 10)
+		{
+			return "Oct " + day;
+		}
+		if(i.getMonth() == 11)
+		{
+			return "Nov " + day;
+		}
+		if(i.getMonth() == 12)
+		{
+			return "Dec " + day;
+		}
+		return null;	
+	}
+	
+	//returns percentage attendance
+	public double getPercent(int time)
+	{
+		double perc = time/75;
+		if(perc >= 1.0)
+		{
+			perc = 1;
+		}
+		perc *= 100;
+		return perc;
+	}
+	
+	@Override
+	public void update(Observable obs, Object arg) 
+	{
+		stud = ((DataSource)obs).getData();
 	}
 }
