052ce4ef8fafe9e0a2784d74eeb3c0b041fb4d24
anoopmakam <anoopmakam@10.180.38.18>
2020-11-29 21:09:29 -0700

    save data from JTable complete.

--- MainProject/src/SaveTable.java
+++ MainProject/src/SaveTable.java
@@ -15,20 +15,24 @@ public class SaveTable
 	
 	public SaveTable(JTable table, File file) throws IOException 
 	{
+		
 		try {
 			
 		
 				TableModel stdTable = table.getModel();
+				System.out.println(stdTable.getColumnCount() + " " + stdTable.getRowCount() );
 				FileWriter out = new FileWriter(file);
 				
-				for(int i = 0; i <= stdTable.getColumnCount(); i++) {
-					out.write(stdTable.getColumnName(i) + ",");
-					
-				}
-				out.write("\n");
+//				for(int i = 0; i < stdTable.getColumnCount() ; i++) {
+//					System.out.println("Column name: " + i);
+//					out.write(stdTable.getColumnName(i) + ",");
+//					
+//				}
+				//out.write("\n");
 				
-				for(int i = 0; i <= stdTable.getRowCount(); i++) {
-					for(int j = 0; i <= stdTable.getColumnCount(); j++) {
+				for(int i = 0; i < stdTable.getRowCount(); i++) {
+					for(int j = 0; j < stdTable.getColumnCount(); j++) {
+						System.out.println("Value : " + i +" "+ j);
 						out.write(stdTable.getValueAt(i,j).toString() + ",");
 					}
 					out.write("\n");
