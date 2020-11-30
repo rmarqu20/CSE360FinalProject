6b436eb42f9d7c3cb91dc7e964bfc734064e18d3
anoopmakam <anoopmakam@10.180.38.18>
2020-11-29 20:44:51 -0700

    save data table

--- MainProject/src/SaveTable.java
+++ MainProject/src/SaveTable.java
@@ -1,38 +1,47 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.*;
 import javax.swing.table.*;
 
 
 
 
-public class SaveTable 
+public class SaveTable
 {
 	
 	
 	public SaveTable(JTable table, File file) throws IOException 
 	{
-		TableModel stdTable = table.getModel();
-		FileWriter out = new FileWriter(file);
-		
-		for(int i = 0; i < stdTable.getColumnCount(); i++) {
-			out.write(stdTable.getColumnName(i) + "\t");
+		try {
 			
+		
+				TableModel stdTable = table.getModel();
+				FileWriter out = new FileWriter(file);
+				
+				for(int i = 0; i <= stdTable.getColumnCount(); i++) {
+					out.write(stdTable.getColumnName(i) + ",");
+					
+				}
+				out.write("\n");
+				
+				for(int i = 0; i <= stdTable.getRowCount(); i++) {
+					for(int j = 0; i <= stdTable.getColumnCount(); j++) {
+						out.write(stdTable.getValueAt(i,j).toString() + ",");
+					}
+					out.write("\n");
+					
+					
+				}
+				out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		out.write("\n");
 		
-		for(int i = 0; i < stdTable.getRowCount(); i++) {
-			for(int j = 0; i < stdTable.getColumnCount(); j++) {
-				out.write(stdTable.getValueAt(i,j).toString() + "\t");
-			}
-			out.write("\n");
-			
-			
-		}
-		out.close();
 		
-		System.out.println("output:" + file);	
-	}	
+	}
 
+	
 }
